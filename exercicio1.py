d42faaa944fa84ebfea245f25001faa75d3b5ea2
luansouzac <luansouzac14@gmail.com>
2025-08-26 21:22:11 -0300

    adiciona contrutor na classe

--- exercicio1.py
+++ exercicio1.py
@@ -1,12 +1,12 @@
 class Pessoa:
-    def _init_(self, nome, idade, peso, altura):
+    def __init__(self, nome, idade, peso, altura):
         self.nome = nome
         self.idade = idade
         self.peso = peso
         self.altura = altura
 
-    def calcula_imc(self, nome, idade, peso, altura):
-        imc = peso / (altura * altura)
+    def calcula_imc(self):
+        imc = self.peso / (self.altura ** 2)
 
         return imc
     
@@ -15,5 +15,7 @@ i = int(input('Digite a sua idade: '))
 p = float(input('Digite o seu peso: '))
 a = float(input('Digite a sua altura: '))
 
-pessoa = Pessoa();
-print(pessoa.calcula_imc(n, i, p, a))
+pessoa = Pessoa(n, i, p, a);    
+print(pessoa.calcula_imc())
+
+print(pessoa.nome)
